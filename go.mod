ae4cf959eddb482e9c0aa3097e6fe0aafe72d08b
lyra <365373011@qq.com>
2022-11-23 23:00:21 +0800

    框架demo的学习

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module spider
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da // indirect
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/djimenez/iconv-go"
 	"github.com/go-resty/resty/v2"
-	"github.com/microcosm-cc/bluemonday"
 )
 
 func main() {
+
 	client := resty.New()
 
 	client.SetBaseURL("https://www.meishichina.com/")
-	//get, err := client.R().EnableTrace().Get("")
+	get, err := client.R().EnableTrace().Get("")
 
-	//if err != nil {
-	//	fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	utfBody, err := iconv.NewReader(res.Body, charset, "utf-8")
+
+	doc, err := goquery.NewDocumentFromReader(utfBody)
+
+	doc.Find(".on li a").Each(func(i int, selection *goquery.Selection) {
+		content := selection.Find("p").Text()
+		fmt.Println(content)
+	})
+
+	//policy := bluemonday.UGCPolicy()
 	//
-	//	return
-	//}
-
-	policy := bluemonday.UGCPolicy()
-
-	sanitize := policy.Sanitize(`<a onblur="alert(secret)" href="http://www.google.com">Google</a>`)
-
-	println(sanitize)
+	//sanitize := policy.Sanitize(`<a onblur="alert(secret)" href="http://www.google.com">Google</a>`)
+	//
+	//println(sanitize)
 
 	//fmt.Println(get)
 }
